101c9a8fdec91f202f91546309b67df2b1f2a6ab
ade reskita <ade.reskita@phincon.com>
2022-04-06 11:23:04 +0700

    Change Error message View from Alert to Label

---
+++ phincon-attendance-app/phincon-attendance-app/Helper/ImageUtils.swift
@@ -0,0 +1,20 @@
+//
+//  ImageUtils.swift
+//  phincon-attendance-app
+//
+//  Created by Ade on 4/6/22.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIImageView{
+    
+    func setImage(_ image: UIImage?, animated: Bool = true) {
+        let duration = animated ? 0.2 : 0.0
+        UIView.transition(with: self, duration: duration, options: .transitionCrossDissolve, animations: {
+            self.image = image
+        }, completion: nil)
+    }
+}

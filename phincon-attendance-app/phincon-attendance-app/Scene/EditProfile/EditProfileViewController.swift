93e3745beda33ac0c8eae603998f3ebcf70d965c
Sri <sriratmurti.official@gmail.com>
2022-04-24 10:37:32 +0700

    Change the view of Edit Profile

--- phincon-attendance-app/phincon-attendance-app/Scene/EditProfile/EditProfileViewController.swift
+++ phincon-attendance-app/phincon-attendance-app/Scene/EditProfile/EditProfileViewController.swift
@@ -20,64 +20,67 @@ protocol EditProfileDisplayLogic: AnyObject
 
 class EditProfileViewController: UIViewController, EditProfileDisplayLogic {
     
-  var interactor: EditProfileBusinessLogic?
-  var router: (NSObjectProtocol & EditProfileRoutingLogic & EditProfileDataPassing)?
-
-  // MARK: Object lifecycle
-  
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-  
-  required init?(coder aDecoder: NSCoder)
-  {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: Setup
-  
-  private func setup()
-  {
-    let viewController = self
-    let interactor = EditProfileInteractor()
-    let presenter = EditProfilePresenter()
-    let router = EditProfileRouter()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-  
-  // MARK: Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
+    var interactor: EditProfileBusinessLogic?
+    var router: (NSObjectProtocol & EditProfileRoutingLogic & EditProfileDataPassing)?
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
+    {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
     }
-  }
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    doSomething()
-  }
-  
-  // MARK: Do something
-  
-  //@IBOutlet weak var nameTextField: UITextField!
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup()
+    {
+        let viewController = self
+        let interactor = EditProfileInteractor()
+        let presenter = EditProfilePresenter()
+        let router = EditProfileRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: Routing
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        doSomething()
+    }
+    
+    // MARK: Do something
+    
+    //@IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var saveButton : UIButton!
-    @IBOutlet weak var tableView : UITableView!
+    @IBOutlet weak var fullnameTextField : UITextField!
+    @IBOutlet weak var idCardTextField : UITextField!
+    @IBOutlet weak var addressTextField : UITextField!
+    @IBOutlet weak var cardView : UIView!
     var data : [EditProfileData] = []
     
     func setupView() {
@@ -86,19 +89,15 @@ class EditProfileViewController: UIViewController, EditProfileDisplayLogic {
         saveButton.setTitle("Save", for: .normal)
         saveButton.titleLabel?.textAlignment = .center
         saveButton.layer.cornerRadius = 8
-            
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(EditProfileTableViewCell.nib(), forCellReuseIdentifier: EditProfileTableViewCell.identifier)
+        
     }
     
-  func doSomething()
-  {
-    let request = EditProfileModel.Put.Request()
-    interactor?.editProfile(request)
-    setupView()
-  }
-  
+    func doSomething() {
+        let request = EditProfileModel.Put.Request()
+        interactor?.editProfile(request)
+        setupView()
+    }
+    
     func presenter(didChange response: EditProfileModel.Put.Response) {
         <#code#>
     }
@@ -115,14 +114,3 @@ class EditProfileViewController: UIViewController, EditProfileDisplayLogic {
     }
 }
 
-extension EditProfileViewController : UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: EditProfileTableViewCell.identifier, for: indexPath) as! EditProfileTableViewCell
-        let profilObject = data[indexPath.row]
-        cell.setupCell(with: profilObject)
-        return cell
-    }
-}
